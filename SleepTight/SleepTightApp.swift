6dfd940bff4d32f3237d484f000cc95cf3d334a1
aarun41 <93056388+aarun41@users.noreply.github.com>
2022-10-21 20:05:42 -0400

    Initial Commit

---
+++ SleepTight/SleepTightApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepTightApp.swift
+//  SleepTight
+//
+//  Created by Aditi Arun on 10/21/22.
+//
+
+import SwiftUI
+
+@main
+struct SleepTightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
